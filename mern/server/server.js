1d89bf15b4183bbc15398985726f77bce5e67124
MarteCodes <jeanmarte93@gmail.com>
2022-10-12 13:13:40 -0400

    draft

--- mern/server/server.js
+++ mern/server/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://Javascript:Javascript123@cluster0.jt19a.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://user7:user717@database.u50ruxf.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
