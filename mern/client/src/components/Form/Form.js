c1b8ab5d41e640977f01e116ee908403c22437b6
Jean Marte <jeanmarte93@gmail.com>
2022-11-01 12:40:24 -0400

    form

--- mern/client/src/components/Form/Form.js
+++ mern/client/src/components/Form/Form.js
@@ -18,24 +18,28 @@ export default function Form() {
 
   }
 
+  const clear = () => { 
+
+  }
+
   return (
     <Paper className={classes.paper}>
-      <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSumbit}>
+      <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSumbit}>
 
       <Typography variant="h6"> Creating a Memory</Typography>
 
-      <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...post, creator: e.target.value })}/>
-      <TextField name="title" variant="outlined" label="title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...post, title: e.target.value })}/>
-      <TextField name="message" variant="outlined" label="message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...post, message: e.target.value })}/>
-      <TextField name="tags" variant="outlined" label="tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...post, tags: e.target.value })}/>
+      <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}/>
+      <TextField name="title" variant="outlined" label="title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })}/>
+      <TextField name="message" variant="outlined" label="message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })}/>
+      <TextField name="tags" variant="outlined" label="tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })}/>
 
       <div className={classes.fileInput}> 
         <FileBase type="file"multiple={false}onDone = {({base64}) => setPostData({ ...postData, selectedFile: base64 })}
         />
       </div>
 
-      <Button className={classes.buttonSubmit} variant="caontained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
-      <Button variant="caontained" color="secondary" size="smal" onClick={clear} fullWidth>Clear</Button>
+      <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+      <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
 
       </form>
     </Paper>
